b4fc4b6726b09cb08f3bc8fd82b539f808fd5904
ameen <ameensayed6780@gmail.com>
2026-01-26 16:05:13 +0530

    fix(cors): make CORS config compatible with local and production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from "url"
 import connectDB from './config/db.js';
 import healthRoutes from './routes/health.js';
 import authRoutes from './routes/auth.js';
@@ -11,6 +13,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 connectDB();
 
@@ -25,6 +29,15 @@ app.use('/api/health', healthRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
+
+// Serve frontend build
+const frontendPath = path.join(__dirname, "../frontend/dist")
+app.use(express.static(frontendPath))
+
+// React SPA fallback
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
